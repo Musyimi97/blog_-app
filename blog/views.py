06f5c503adfae262ea950d69a78777a900b7375c
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-14 13:48:17 +0300

    new class

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+class Blog(models.Model):
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
+
