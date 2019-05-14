a40fbee5a713e369cee975aade83c38a17f57d57
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-14 13:49:42 +0300

    two deletions

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
 
 # Create your views here.
-class Blog(models.Model):
-    
-
-    def __str__(self):
-        return 
-
-    def __unicode__(self):
-        return 
 
